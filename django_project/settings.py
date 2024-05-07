fa8ba130769bd548f845c5b1e417ea63c1234ddd
aaronmasm <aaronmasm85@gmail.com>
2024-05-07 14:12:04 -0500

    New updates for Heroku deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-p5iw&^p$opqg7a=5&b5qusmz^&^jyzovwn30aas&c2%q4i4&uk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
